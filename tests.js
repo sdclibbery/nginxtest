df842d385ff12996f4faeefe93813231e9bd7eca
Stephen Clibbery <sdclibbery@googlemail.com>
2016-08-20 23:47:04 +0100

    clean up debug logging

--- tests.js
+++ tests.js
@@ -1,11 +1,14 @@
 const spawn = require('child_process').spawn;
 const http = require('http');
 
+var dbg = ()=>0;
+//var dbg = console.log;
+
 
 function mockServer(host, port) {
   var path, response, cb;
   var server = http.createServer( (req, res) => {
-console.log('request received '+req.url);
+    dbg('request received '+req.url);
     if (req.url === path) {
       res.writeHead(response.code, {'Content-Type': 'text/plain'});
       res.end(response.body);
@@ -18,7 +21,7 @@ console.log('request received '+req.url);
     on: function (p, r) {
       path = p;
       response = r;
-      return { and: (cb) => { console.log('listening'); server.listen(port, host, cb); return server } }
+      return { and: (cb) => { dbg('listening'); server.listen(port, host, cb); return server } }
     },
   };
 };
@@ -27,7 +30,7 @@ var thenRespond = (c, b) => ({ code: c, body: b });
 var stop = function () {
   var args = [].slice.call(arguments, 0);
   return function() {
-    console.log('closing');
+    dbg('closing');
     spawn('nginx/nginx', ['-c', 'nginx.conf', '-s', 'stop']);
     args.map((server) => server.close());
   };
@@ -37,7 +40,7 @@ function startNginx () {
   return spawn('nginx/nginx', ['-c', 'nginx.conf']);
 };
 function request(url) {
-console.log('request');
+  dbg('request');
   var req = {};
   http.get(url, (res) => {
     var body = '';
@@ -46,11 +49,11 @@ console.log('request');
       body += chunk;
     });
     res.on('end', () => {
-console.log('reqdone');
+      dbg('reqdone');
       req.callback(res.statusCode, body);
     });
   }).on('error', (e) => {
-console.log('reqerr: '+e);
+    dbg('reqerr: '+e);
     req.callback(1000, e);
   });
   return req;
@@ -69,8 +72,8 @@ function expect (req) {
   var result = {
     code: (c) => { expected.code = c; return result; },
     body: (b) => { expected.body = b; return result; },
-    then: (cb) => { console.log('then');req.callback = (code, body) => {
-console.log('testing');
+    then: (cb) => { dbg('then');req.callback = (code, body) => {
+      dbg('checking');
       assert(expected.code, code, 'code');
       assert(expected.body, body, 'body');
       cb();
@@ -81,16 +84,17 @@ console.log('testing');
 var is = (x) => x;
 
 
-function proxiesHomepageToLocalhost () {
+var tests = {};
+
+tests.proxiesHomepageToLocalhost = function () {
   var server = mockServer('127.0.0.1', 8080).on("/", thenRespond(200, "homepage")).and(()=>{
-console.log('in test');
     startNginx("live");
     expect(request("http://localhost:80/")).code(is(200)).body(is("homepage")).then(stop(server));
   });
 };
 
 /*
-function proxiesClassifiedsToMarketplaceWeb () {
+tests.proxiesClassifiedsToMarketplaceWeb = function () {
   mockDns().on("beta.marketplace.thehutgroup.local", goto("127.0.0.2"));
   mockServer("127.0.0.2", 8080).on("/classifieds/pets/cats/all/uk", thenRespond(200, "classifieds"));
   startNginx("live");
@@ -99,4 +103,7 @@ function proxiesClassifiedsToMarketplaceWeb () {
 };
 */
 
-proxiesHomepageToLocalhost();
+Object.keys(tests).map(function(key) {
+   console.log('TEST '+key);
+   tests[key]();
+});
